ddc83c3ad4dfd8e8805e68fde55377e7987f40d9
Vinícius Gabriel <timelord1025@gmail.com>
2021-07-01 22:03:01 +0000

    added basic gps check

---
+++ test/gps.test.ts
@@ -0,0 +1,6 @@
+import { getGPSdata } from "../Scripts/AVL Data Parser/GPSelement"
+
+test("Test online value", () => {
+    let parsed = getGPSdata(0x209cca80)
+    expect(parsed).toBeCloseTo(54.7146368)
+})
