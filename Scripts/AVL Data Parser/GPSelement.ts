399a9e6a1f2ba5cc3ef628752200b3a1c435998d
Vinícius Gabriel <timelord1025@gmail.com>
2021-07-01 22:04:06 +0000

    fixed implementation in getGPSdata to fix negative values

--- Scripts/AVL Data Parser/GPSelement.ts
+++ Scripts/AVL Data Parser/GPSelement.ts
@@ -1,15 +1,15 @@
 import { PacketReader } from "../PacketReader";
 
 export interface IGPSelement {
-    Longitude : number,
-    Latitude : number,
-    Altitude : number,
-    Angle : number,
-    Satellites : number,
-    Speed : number
+    Longitude: number,
+    Latitude: number,
+    Altitude: number,
+    Angle: number,
+    Satellites: number,
+    Speed: number
 }
 
-const to_binary = (value : number) => (value >>> 0).toString(2)
+const to_binary = (value: number) => (value >>> 0).toString(2)
 
 function ProcessReal(value: number) {
     let binary = to_binary(value)
@@ -18,27 +18,49 @@ function ProcessReal(value: number) {
     return value / 10000000
 }
 
-function getGPSdata (num: number) {
-    //let num = parse('20 9c ca 80')
-    //expect(num).toBe(547146368)
-    let binary = num.toString(2)
-    while (binary.length % 8 != 0) {
-        binary = "0" + binary
-    }
-    //let binary = to_binary(num)
-    //expect(binary).toBe('00100000100111001100101010000000')
-    if (binary.substr(0, 1) != '0') {
-        num *= -1
-    }
-    return num *  0.0000001
-    //expect(result).toBeCloseTo(54.714636)
+export function getGPSdata(gum: number) {
+    gum <<= 32; 
+    gum >>= 32;
+    gum = gum / 10000000;
+    return gum;
 }
 
-export function isIGPSelement (obj: any) : obj is IGPSelement {
+// export function getGPSdata(num: number) {
+//     let bin = num.toString(2)
+//     let [start, end] = [bin.slice(0, 1), bin.slice(1)]
+//     if (start == '1') {
+//         if (num >= 0) num = -num
+//     } else {
+//         if (num <= 0) num = -num
+//     }
+//     let numStr = num.toString()
+//     let i = 2
+//     if (num < 0) i = 3
+//     num = Number(numStr.slice(0, i) + '.' + numStr.slice(i))
+//     return num
+// }
+
+// export function getGPSdata(num: number) {
+//     //let num = parse('20 9c ca 80')
+//     //expect(num).toBe(547146368)
+//     let binary = num.toString(2)
+//     while (binary.length % 8 != 0) {
+//         binary = "0" + binary
+//     }
+//     //let binary = to_binary(num)
+//     //expect(binary).toBe('00100000100111001100101010000000')
+//     if (binary.substr(0, 1) != '0') {
+//         num *= -1
+//     }
+//     return num * 0.0000001
+//     //expect(result).toBeCloseTo(54.714636)
+// }
+
+export function isIGPSelement(obj: any): obj is IGPSelement {
     return obj && typeof obj.Angle === 'number' && typeof obj.Longitude === 'number' && typeof obj.Latitude === 'number'
 }
 
-export function defaultGPSElement (longitude: number = 0, latitude: number = 0) : IGPSelement {
+export function defaultGPSElement(longitude: number = 0, latitude: number = 0): IGPSelement {
     return {
         Latitude: latitude,
         Longitude: longitude,
